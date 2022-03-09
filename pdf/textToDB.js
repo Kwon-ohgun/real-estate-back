f75ac725c3a47e1ecb10e9b62905538f181defc0
ohgun <dhrjs0128@naver.com>
2022-03-09 17:47:44 +0900

    [#1] 소속과 이름 데이터 생성

--- pdf/textToDB.js
+++ pdf/textToDB.js
@@ -5,7 +5,7 @@ const lineArray = article.toString().split('\n');
 const dataArray = [];
 const stringArray = [];
 let strings = [];
-// 소속마다 문자 자르기
+// 소속마다 문자 분리
 for (let i = 0; i < lineArray.length; i++) {
   const line = lineArray[i];
   if (line.startsWith('소속')) {
@@ -16,15 +16,26 @@ for (let i = 0; i < lineArray.length; i++) {
     stringArray.push(strings);
   }
 }
+
+// 소속과 이름 데이터 생성
+for (let i = 0; i < stringArray.length; i++) {
+  const strings = stringArray[i];
+  for (let j = 0; j < strings.length; j++) {
+    const line = strings[j];
+    // 소속에서 이름 빼기
+    if (line.startsWith('소속')) {
+      const belongs = line.split(' ');
+      dataArray.push({
+        team: belongs[1] + (belongs[2] !== '직위' ? ` ${belongs[2]}` : '') + (belongs[2] !== '직위' && belongs[3] !== '직위' ? ` ${belongs[3]}` : '') + (belongs[2] !== '직위' && belongs[3] !== '직위' && belongs[4] !== '직위' ? ` ${belongs[4]}` : ''),
+        name: belongs[belongs.length - 1]
+      })
+    }
+  }
+}
+
 // for (let i = 0; i < lineArray.length; i++) {
 //   const line = lineArray[i];
 //   if (line.startsWith('소속')) {
-//     strings = [];
-//     const belongs = line.split(' ');
-//     dataArray.push({
-//       team: belongs[1],
-//       name: belongs[5]
-//     })
 //   }
 // }
-console.log(stringArray);
+console.log(dataArray);
